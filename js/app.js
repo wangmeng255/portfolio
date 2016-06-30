3e97fe1df8f6c471bd4b2376d4fd8cedcbcedaed
wangmeng <wangmeng255@gmail.com>
2016-06-30 12:36:50 -0700

    add @media

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 "use strict"
 $(function() {
+  //add fonts to head
+  $("head").append("<link href='//fonts.googleapis.com/css?family=Cinzel+Decorative'" + 
+                   " rel='stylesheet' type='text/css'>");
+  $("head").append("<link href='//fonts.googleapis.com/css?family=Dosis'" + 
+                   " rel='stylesheet' type='text/css'>");
+  $("head").append("<link href='//fonts.googleapis.com/css?family=Amiri'" + 
+                   " rel='stylesheet' type='text/css'>");
   //add position:sticky to #topbar
   var topbar = document.getElementById("topbar");
   Stickyfill.add(topbar);
 
   $(window).scroll(function(event) {
     var topbar = $("#topbar");
-    console.log(topbar.offset().top);
-    var rem = $(window).width() / parseFloat($("body").css("font-size"));
+    var rem = parseFloat($("body").css("font-size"));
     if(topbar.offset().top > rem * 37.5)
       topbar.height(rem * 3);
-    console.log(rem * 3);
+    else topbar.height(1);
   });
 
   $("header").click(function() {
