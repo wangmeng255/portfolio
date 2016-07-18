5a239d3ddbc63532bbab95b5d176c6c285947ace
wangmeng <wangmeng255@gmail.com>
2016-07-18 16:55:05 -0700

    fix stickyfill and move link to html head

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
 "use strict"
 $(function() {
-  //add fonts to head
-  $("head").append("<link href='//fonts.googleapis.com/css?family=Cinzel+Decorative'" + 
-                   " rel='stylesheet' type='text/css'>");
-  $("head").append("<link href='//fonts.googleapis.com/css?family=Dosis'" + 
-                   " rel='stylesheet' type='text/css'>");
-  $("head").append("<link href='//fonts.googleapis.com/css?family=Amiri'" + 
-                   " rel='stylesheet' type='text/css'>");
   //add position:sticky to #topbar
   var topbar = document.getElementById("topbar");
   Stickyfill.add(topbar);
+  setTopbarHeight();
 
   $(window).scroll(function(event) {
-    var topbar = $("#topbar");
-    var rem = parseFloat($("body").css("font-size"));
-    if(topbar.offset().top > rem * 37.5)
-      topbar.height(rem * 3);
-    else topbar.height(1);
+    setTopbarHeight();
   });
 
   $("header").click(function() {
     $(window).scrollTop(0);
   });
 });
+
+function setTopbarHeight() {
+  var topbar = $("#topbar");
+  var rem = parseFloat($("body").css("font-size"));
+  if(topbar.offset().top > rem * 37.5)
+    topbar.height(rem * 3);
+  else topbar.height(1);
+}
